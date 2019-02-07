f0f6e03fb241fcd7dd39ae446cd06f9707473b10
jackfriesen <jack_friesen@yahoo.com>
2019-02-07 11:01:46 -0600

    Demo 1 Feb 7th

--- 01/sketch.js
+++ 01/sketch.js
@@ -5,11 +5,21 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x=45;
+let xSpeed=6;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  fill('red');
+  ellipse(x, 200, 125);
+  fill(0);
+  rect(mouseX, 400, 75, 75);
+
+  x+=xSpeed;
+  if(x>windowWidth) x=-30;
 }
