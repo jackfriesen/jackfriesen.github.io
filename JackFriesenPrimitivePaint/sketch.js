9c662c018c0334b8d46a7366b765a9668d62d0a8
jackfriesen <jack_friesen@yahoo.com>
2019-02-12 11:09:47 -0600

    primitive paint

--- JackFriesenPrimitivePaint/sketch.js
+++ JackFriesenPrimitivePaint/sketch.js
@@ -1,9 +1,12 @@
-// Project Title
-// Your Name
-// Date
+// Primitive Paint    
+// Jack Friesen
+// Tuesday, February 12
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+//program is designed to allow the user to draw shapes 
+//of random color on a canvas  
+
+let rectSize = 50;
+let rectSpeed = 1;
 
 //create canvas
 function setup() {
@@ -11,26 +14,36 @@ function setup() {
   background(0);
 }
 
-//write name
 function draw() {
+  //write name
   textFont("Times New Roman");
   textSize(30);
   fill(255);
-  text("Jack Friesen", windowWidth-160, windowHeight-10);
+  text("Jack Friesen", windowWidth - 160, windowHeight - 10);
+
+  //draw autonomous shape
+  fill(random(255), random(255), random(255));
+  rect(windowWidth / 2, windowHeight / 2, rectSize, rectSize);
+  if(rectSize > 100 || rectSize < 50) {
+    rectSpeed *= -1;
+  }
+  
+  rectSize += rectSpeed;
+
 }
 
 //draw shapes via user input
 function keyTyped() {
-  if(key==="a") {
+  if (key === "a") {
     fill(random(255), random(255), random(255));
-    rect(mouseX, mouseY, 50, 40); 
+    rect(mouseX, mouseY, 50, 40);
   }
-  if(key==="s") {
+  if (key === "s") {
     fill(random(255), random(255), random(255));
     ellipse(mouseX, mouseY, 50, 40);
   }
-  if(key==="d") {
+  if (key === "d") {
     fill(random(255), random(255), random(255));
-    triangle(mouseX-20, mouseY+20, mouseX-20, mouseY-20, mouseX+20, mouseY+20);
+    triangle(mouseX - 20, mouseY + 20, mouseX - 20, mouseY - 20, mouseX + 20, mouseY + 20);
   }
 }
