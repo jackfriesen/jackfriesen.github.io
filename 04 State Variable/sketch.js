b803bca9f99ed2f3fd86fadb0137b5c780a95140
jackfriesen <jack_friesen@yahoo.com>
2019-02-13 10:42:01 -0600

    Feb 13
    checked square shape to have it move clockwise around the canvas

---
+++ 04 State Variable/sketch.js
@@ -0,0 +1,42 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let state = 0; //0 = top, 1 = right, 2 = bottom, 3 = left
+const rectSize = 30;
+let x = 0;
+let y = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  if(state === 0) { //on top, going right
+    x += 5;
+    if(x >= windowWidth - rectSize){
+      state = 1;
+    } 
+  }else if (state === 1) { //on right going down
+    y += 5;
+    if(y >= windowHeight - rectSize){
+      state = 2;
+    } 
+  }else if(state ===2){ //on bottom going left
+    x -= 5;
+    if(x <= 0) {
+      state = 3;
+    } 
+  }else if (state === 3){
+    y -= 5;
+    if(y <= 0){
+      state = 0;
+    }
+  }
+
+  rect(x, y, rectSize, rectSize);
+}
