912e942422bec81c5ddba99946b833fb50918170
jackfriesen <jack_friesen@yahoo.com>
2019-02-13 11:03:11 -0600

    feb 13
    primitive paint

--- JackFriesenPrimitivePaint/sketch.js
+++ JackFriesenPrimitivePaint/sketch.js
@@ -11,17 +11,18 @@ let rectSpeed = 1;
 //create canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  
 }
 
 function draw() {
+  background(0);
   //write name
   textFont("Times New Roman");
   textSize(30);
   fill(255);
   text("Jack Friesen", windowWidth - 160, windowHeight - 10);
 
-  //draw autonomous shape
+  //draw autonomous flashy shape
   fill(random(255), random(255), random(255));
   rect(windowWidth / 2, windowHeight / 2, rectSize, rectSize);
   if(rectSize > 100 || rectSize < 50) {
@@ -32,18 +33,32 @@ function draw() {
 
 }
 
+function prev() {
+  fill(random(255), random(255), random(255));
+  rect(mouseX, mouseY, 50, 40);
+}
+
+
 //draw shapes via user input
 function keyTyped() {
+  //draw rectangle
   if (key === "a") {
-    fill(random(255), random(255), random(255));
-    rect(mouseX, mouseY, 50, 40);
+    prev();
+    if(mouseIsPressed) {
+      rect(mouseX, mouseY, 50, 40);
+    }
   }
+  //draw oval
   if (key === "s") {
     fill(random(255), random(255), random(255));
     ellipse(mouseX, mouseY, 50, 40);
   }
+  //draw triangle
   if (key === "d") {
     fill(random(255), random(255), random(255));
     triangle(mouseX - 20, mouseY + 20, mouseX - 20, mouseY - 20, mouseX + 20, mouseY + 20);
+  }
+  if (key === " "){
+    background(0);
   }
 }
