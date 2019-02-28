ec24bb412434310d269c06c73f4437fb29c94305
jackfriesen <jack_friesen@yahoo.com>
2019-02-28 11:02:50 -0600

    feb 28
    rollovers

---
+++ 07 Loops Demo/sketch.js
@@ -0,0 +1,44 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let x = 150;
+let y = 150;
+let colors = [];
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colors.push(color(255, 0, 0));
+  colors.push(color(255));
+  colors.push(color(255, 0, 0));
+  colors.push(color(255));
+  colors.push(color(255, 0, 0));
+}
+
+function drawTargetWhile() {
+  let diameter = 200;
+  let counter = 0;
+  while (counter < 5) {
+    ellipse(width / 2, height / 2, diameter, diameter);
+    diameter -= 40;
+    counter++;
+  }
+}
+
+function drawTargetFor() {
+
+  for (let i = 5; i > 0; i--) {
+    fill(colors[i-1]);
+    ellipse(x, y, i * 40, i * 40);
+  }
+}
+
+function draw() {
+  background(220);
+  //drawTargetWhile();
+  drawTargetFor();
+}
--- JackFriesenRollovers2/sketch.js
+++ JackFriesenRollovers2/sketch.js
@@ -19,28 +19,10 @@
 
 //TO DO
 //
-//set values for rgb variables for quadrants
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//mousePressed function created and given appropriate tasks
+//
+//while quadrant = 1 have black squares then initialize fade
+//use while loop to fade back to red
 
 
 
@@ -54,13 +36,20 @@
 let quadrant = 1; //top left=1 top right=2 bottom left=3 bottom right=4
 
 let r1 = 255; //rgb values for quad 1
-let g1 =0;
+let g1 = 0;
 let b1 = 0;
 
+let r2 = 255; //rgb values for quad 2
+let g2 = 0;
+let b2 = 0;
 
-let r2, g2, b2; //rgb values for quad 2
-let r3, g3, b3; //rgb values for quad 3
-let r4, g4, b4; //rgb values for quad 4
+let r3 = 255; //rgb values for quad 3
+let g3 = 0;
+let b3 = 0;
+
+let r4 = 255; //rgb values for quad 4
+let g4 = 0;
+let b4 = 0;
 
 
 
@@ -73,28 +62,96 @@ function setup() {
 
 function draw() {
   drawSquares();
+  rollOvers();
+  fade();
 }
 
-//draw squares and do fade colors
+function mousePressed() {
+  while (quadrant === 1) {
+
+  }
+}
+
+function fade() {
+  //fade quad 1
+  if (quadrant === 1) {
+    g1 = 255;
+  }
+  else {
+    if (g1 > 0) {
+      g1 -= 10;
+    }
+  }
+
+  //fade quad 2
+  if (quadrant === 2) {
+    g2 = 255;
+  }
+  else {
+    if (g2 > 0) {
+      g2 -= 10;
+    }
+  }
+
+  //fade quad 3
+  if (quadrant === 3) {
+    g3 = 255;
+  }
+  else {
+    if (g3 > 0) {
+      g3 -= 10;
+    }
+  }
+
+  //fade quad 4
+  if (quadrant === 4) {
+    g4 = 255;
+  }
+  else {
+    if (g4 > 0) {
+      g4 -= 10;
+    }
+  }
+}
+
+//check for rollovers and change quadrant state
+function rollOvers() {
+  if (mouseX < width / 2) {
+    if (mouseY < height / 2) {
+      quadrant = 1; // top left
+    }
+    else {
+      quadrant = 3; //bottom left
+    }
+  }
+
+  if (mouseX > width / 2) {
+    if (mouseY < height / 2) {
+      quadrant = 2; //top right
+    }
+    else {
+      quadrant = 4; //bottom right 
+    }
+  }
+}
+
+//draw squares and do starting colors
 function drawSquares() {
   //quad 1
   fill(r1, g1, b1);
   rect(0, 0, width / 2, height / 2); //quad 1
- 
+
   //quad 2
-  fill(r2, g2 b2);
+  fill(r2, g2, b2);
   rect(width / 2, 0, width / 2, height / 2); //quad 2
-  
+
   //quad 3
   fill(r3, g3, b3);
   rect(0, height / 2, width / 2, height / 2); //quad 3
-  
+
   //quad 4
   fill(r4, g4, b4);
   rect(width / 2, height / 2, width / 2, height / 2); //quad 4
 }
 
-//check for rollovers and fade
-function rollOvers() {
 
-}
