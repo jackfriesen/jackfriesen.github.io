73e114425094a9df4cf9a34f39c22b2ba092706e
jackfriesen <jack_friesen@yahoo.com>
2019-03-07 11:01:36 -0600

    mar 7
    multicolordgrid

--- JackFriesenMultiColoredGrid/sketch.js
+++ JackFriesenMultiColoredGrid/sketch.js
@@ -6,16 +6,63 @@
 // - describe what you did to take this project "above and beyond"
 
 
-//three states 
+//TO DO 
+//
+//set square size as a variable that can also be 
+//included in the x for loop in makeSquares() so it only 
+//prints squares that far
 
 
-
-
+let change = false;
+let squareSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  squareSize = 50;
+
+  noLoop();
 }
 
+//resize sketch when canvas resized
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+//draw squares for the first time
 function draw() {
-  background(220);
+  makeSquares();
+  if (change) {
+    //squares get smaller
+    if (mouseButton === LEFT) {
+      squareSize -= 5;
+    }
+    //squares get bigger
+    if (mouseButton === RIGHT) {
+      //squares get bigger
+      squareSize += 5;
+    }
+  }
+
+}
+
+//refresh canvas if key is pressed
+function keyPressed() {
+  change = false;
+  draw();
+}
+
+function mousePressed() {
+  change = true;
+  draw();
+}
+
+//make the squares on the screen
+function makeSquares() {
+  for (let x = 0; x <= width - squareSize; x += squareSize) {
+    for (let y = 0; y <= height - squareSize; y += squareSize) {
+      fill(int(random(200, 255)), 0, int(random(100, 150)));
+      rect(x, y, squareSize, squareSize);
+    }
+  }
 }
