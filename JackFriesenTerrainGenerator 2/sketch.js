97324d8c9fefa8360302fded0982dd71622f2e0b
jackfriesen <jack_friesen@yahoo.com>
2019-03-14 11:02:44 -0600

    mar 14

--- JackFriesenTerrainGenerator 2/sketch.js
+++ JackFriesenTerrainGenerator 2/sketch.js
@@ -2,32 +2,30 @@
 //
 //Generates random terrain using perlin noise;
 
-
+let inc = 0.01;
+let start = 0;
+let rectWidth = 5;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  generateTerrain();
-  drawFlag();
+  //noLoop();
 }
 
 function draw() {
-
+  background(255);
+  fill(0);
+  generateTerrain();
 }
 
 function generateTerrain() {
-  let yOff = 0;
-  let x = 0;
-  for (let i = 0; i <= width; i++) {
-    let y = map(noise(yOff), 0, 1, 0, height);
-
-    yOff += 0.001;
-
-    fill(0);
-    rect(x, y, 2, height - y);
-
-    x += 1;
+  let xOff = start;
+  for (let x = 0; x < width; x++) {
+    let y = noise(xOff) * height;
+    rect(x, y, rectWidth, height - y);
+    xOff += inc;
   }
+  start += inc;
 }
 
 function keyPressed() {
@@ -40,5 +38,5 @@ function drawFlag(x, y) {
   let rectY = 20;
   fill(0);
   rect(rectX, rectY, 1, 20);
-  triangle(rectX + 1, rectY + 1, rectX, rectY - 5, rectX + 12, rectY - 5/2);
+  triangle(rectX + 1, rectY + 1, rectX, rectY - 5, rectX + 12, rectY - 5 / 2);
 }
