2f30703e52bdc33e9ca0fa6e93bad524c5129895
jackfriesen <jack_friesen@yahoo.com>
2019-04-16 10:54:03 -0600

    april 16
    -generative art

--- JackFriesenGenerativeArt/sketch.js
+++ JackFriesenGenerativeArt/sketch.js
@@ -7,22 +7,25 @@
 //zoom out of browser window if you can't see full image
 //bc canvas is too big
 
-let frameOneW;
-let frameOneH;
+//TO DO
+//
+//CHOOSE A FILL
+//
+//fix line y2
+//
+//fix save() function
+
+let circSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noLoop();
-  frameOneW = width / 2 + 10;
-  frameOneH = height / 2 + 10
+  circSize = random(60, 80);
 }
 
 function draw() {
-  drawFrameOne();
-  drawFrameTwo();
-  
-  
-  
+  background(0);
+  drawCircles();
 }
 
 function keyPressed() {
@@ -31,20 +34,19 @@ function keyPressed() {
   }
 }
 
-function drawFrameOne() {
-  push();
-  strokeWeight(7);
-  rect(5, 5, frameOneW, frameOneH);
-  pop();
+function drawCircles() {
+  for(let x = 40; x < width; x += circSize + 10) {
+    push();
+    stroke(150);
+    strokeWeight(3);
+    line(x, 0, x, height - circSize);
+    pop();
+    for(let y = 20; y < height - circSize; y += circSize) {
+      fill(random(255), random(100), 55);
+      fill(random(245), random(90), 45);
+      //fill(random(245), random(80), 35);
+      ellipse(x, y, circSize, circSize);
+      circSize = random(20, 60);
+    }
+  }
 }
-
-function drawFrameTwo() {
-  push();
-  strokeWeight(3);
-  translate(width / 2 + 2, 3);
-  rect(0, 0, width / 4, height / 4);
-  pop();
-}
-
-
-
