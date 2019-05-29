5cf182aa2de6ba1e7b05cc935f685f87adb31824
jackfriesen <jack_friesen@yahoo.com>
2019-05-29 11:03:42 -0600

    FINAL
    -worked on collisions

--- final/sketch.js
+++ final/sketch.js
@@ -1,35 +1,97 @@
 // Final
 //puzzle/platformer
 
-//MAKE 8 BIT ART HERE
-// https://make8bitart.com/
 
-//floors hex is 664C4C & 4C3333
 
-let floor = [];
+//FIND TEMPORARY OBJECTS AND MAKE ART AFTER
+
+
+
+
+
+//TODAY
+//
+//CREATE ENVIRONMENT LEVEL 1 WITH HIT BOXES
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let imgArray = [];
+let platformImg = [];
+let platforms = [];
+let hit = false;
+
+function preload() {
+  platformImg.push(loadImage("assets/platform.png"));
+
+  //load right states
+  for (let i = 0; i < 4; i++) {
+    imgArray.push(loadImage("assets/Bowser_right" + i + ".png"));
+  }
+
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  imageMode(CENTER);
-  floor.push(loadImage("assets/floor.png"));
+  platforms.push(new Platform(width / 2, height / 2));
 }
 
 function draw() {
   background(0);
-  image(floor[0], width /2, height /2);
+  //rect(mouseX, mouseY, 55, 58);
+  image(imgArray[0], mouseX, mouseY);
+
+  //rect(width / 2, height / 2, 180, 15);
+  platforms[0].display();
+
+  hit = collideRectRect(width / 2, height / 2, 180, 15, mouseX, mouseY, 55, 58);
+  print(hit);
+
 }
+
+
 
 
 class Hero {
 
 }
 
+class Platform {
+  //Constructor and Class Properties
+  constructor(x_, y_) {
+    this.x = x_;
+    this.y = y_;
+    this.w = platformImg[0].width;
+    this.h = platformImg[0].height;
+  }
 
-//PUZZLE IDEAS
+  //Class Methods
 
-//Block Puzzle
-//-push blocks around to reach heights you cannot jump to or to stay 
-//on switches so you can go through a door
+  display() {
+    image(platformImg[0], this.x, this.y);
+  }
+}
 
-//skylanders flip puzzle
 
