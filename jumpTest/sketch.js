403ebd5b1a7b6cb2d4881d5ae290a5dfc8d8060b
Jack Friesen <jack_friesen@yahoo.com>
2019-05-31 16:01:54 -0600

    jump testing
    -created jump animation on a rectangle to transfer over to hero
    -may also use left/right movement animations created in the process but need to be able to hold key down for continuous movement

--- jumpTest/sketch.js
+++ jumpTest/sketch.js
@@ -1,8 +1,33 @@
 // testing jump animation for hero
 
-let context, rectangle, loop;
+
+
+
+
+
+
+
+//CREATE SMOOTHER MOVEMENTS LEFT AND RIGHT 
+// (when friction is commented out the square can move of its own)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let jumping = true;
 let rectW = 32;
+let rectH = 32;
 let rectX = 144;
 let xVelocity = 0;
 let rectY = 0;
@@ -10,15 +35,46 @@ let yVelocity = 0;
 
 function setup() {
   createCanvas(320, 180);
+  noStroke();
 }
 
 function draw() {
-  background(220);
+  background(50);
+  fill(255, 0, 0);
+  rect(rectX, rectY, rectW, rectH);
+
+  yVelocity += 1.5; //gravity
+  rectX += xVelocity;
+  rectY += yVelocity;
+  xVelocity *= 0.9; //friction
+  yVelocity *= 0.9; //friction
+
+  if(rectY > height - rectH - 2) {
+    jumping = false;
+    rectY = height-rectH - 2;
+    yVelocity = 0;
+  }
+
+  if(rectX < 0 - rectW) {
+    rectX = width - rectW;
+  }
+  if(rectX > width) {
+    rectX = 1;
+  }
 }
 
-function controller () {
-  let left = false;
-  let right = false;
-  let up = false;
-  
+function keyPressed() {
+  if(keyCode === LEFT_ARROW) {
+    xVelocity -= 5;
+  }
+  if(keyCode === RIGHT_ARROW) {
+    xVelocity += 5;
+  }
+  if(keyCode === UP_ARROW && jumping === false) {
+    yVelocity -= 20;
+    jumping = true;
+  }
+  if(keyCode === DOWN_ARROW) {
+
+  }
 }
