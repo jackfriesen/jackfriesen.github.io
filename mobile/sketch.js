e461f55db7d4c51599f85ebb5eea03ca1b215ab2
jackfriesen <47328954+jackfriesen@users.noreply.github.com>
2019-07-13 13:01:00 -0600

    movement via touch experiment

--- mobile/sketch.js
+++ mobile/sketch.js
@@ -1,12 +1,32 @@
 // my first mobile game
 
+let x ;
+let xSpeed = 0;
+let y;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  y = height / 3;
+  x = width / 2;
 }
 
 function draw() {
   background(0);
   fill(255);
   text(accelerationX, width / 2, height / 2);
+  if(accelerationX > 0) {
+    x += 3;
+  }
+  else if(accelerationX < 0) {
+    x -= 3;
+  }
+
+  if(x > width) {
+    x = 0;
+  }
+  else if (x < 0) {
+    x = width - 50;
+  }
+
+  rect(x, y, 50, 50);
 }
